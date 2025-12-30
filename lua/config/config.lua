8eb6609fb0b78788a7c0307f6d29438ed52b65cd
Ryan Stewart <ryanstew@users.noreply.github.com>
2025-12-30 15:36:27 -0800

    width indicators and -ro for comment formatting

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -11,4 +11,14 @@ opt.signcolumn = "number"
 opt.splitright = true
 opt.splitbelow = true
 opt.shellpipe = ">%s 2>&1"
+opt.textwidth = 100
+opt.colorcolumn = "100"
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    -- Never allow comment extension, yuck
+    vim.opt.formatoptions:remove 'r'
+    vim.opt.formatoptions:remove 'o'
+  end,
+})
 
