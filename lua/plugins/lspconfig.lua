d08fbca0157c61313ee4902d9144a195feaa4d29
Ryan Stewart <ryanstew@users.noreply.github.com>
2026-01-16 12:19:48 -0800

    add mason

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,34 +1,43 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    vim.lsp.config("zls", {
-      semantic_tokens = 'partial',
-      enable_build_on_save = true,
-    })
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.config("zls", {
+        semantic_tokens = 'partial',
+        enable_build_on_save = true,
+      })
 
-    vim.lsp.enable('zls')
-    vim.lsp.enable('rust_analyzer')
-    vim.lsp.enable('jedi_language_server')
-    vim.lsp.enable('c3_lsp')
+      vim.lsp.enable('zls')
+      vim.lsp.enable('rust_analyzer')
+      vim.lsp.enable('c3_lsp')
 
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'koto',
-      callback = function()
-        vim.lsp.start({
-          cmd = { "koto-ls" },
-          root_dir = vim.fn.getcwd(),
-        })
-      end,
-    })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'koto',
+        callback = function()
+          vim.lsp.start({
+            cmd = { "koto-ls" },
+            root_dir = vim.fn.getcwd(),
+          })
+        end,
+      })
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(args)
-        local opts = { buffer = args.buf }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "g.", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "cd", vim.lsp.buf.rename, opts)
-      end,
-    })
-  end,
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local opts = { buffer = args.buf }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "g.", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "cd", vim.lsp.buf.rename, opts)
+        end,
+      })
+    end,
+  },
+  {
+    'mason-org/mason-lspconfig.nvim',
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    }
+  }
 }
