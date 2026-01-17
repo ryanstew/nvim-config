2bb710de6369e896d2ad6a3a588a1b4fc344cf30
Ryan Stewart <ryanstew@users.noreply.github.com>
2026-01-16 17:07:15 -0800

    trouble keybinds

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,4 +2,21 @@ return {
   'folke/trouble.nvim',
   cmd = "Trouble",
   opts = {},
+  keys = {
+    {
+      '<leader>ee',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = "Show buffer diagnostics",
+    },
+    {
+      '<leader>eE',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = "Show all diagnostics",
+    },
+    {
+      '<leader>bs',
+      '<cmd>Trouble symbols toggle focus=false<cr>',
+      desc = "Show symbols",
+    },
+  }
 }
